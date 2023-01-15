ccee918527d79919cc5a74f2c2652e99a05e9a3d
OriSketchy <orisketchy@gmail.com>
2023-01-15 16:28:42 +0000

    Initial Version

---
+++ Assets/2D Character Controller/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Update is called once per frame
+    //Get input from player
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+       if (Input.GetButtonDown("Jump"))
+       {
+            jump = true;
+       }
+
+        if(Input.GetButtonDown("Crouch")) 
+       {
+            crouch = true;
+       } else if (Input.GetButtonUp("Crouch"))
+       {
+            crouch = false;
+       }
+        
+    }
+
+    //Update is called a fixed amount of times per second
+    //Apply input from player
+    void FixedUpdate ()
+    {
+        //Move Character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+        //Multiplying by Time.fixedDeltatime ensures that the player moves the same amount no matter how often this function is called
+        //jump = false; reverts jump back to Not currently happening so the player only jumps once and then stops jumping 
+    }
+
+}
+
+
