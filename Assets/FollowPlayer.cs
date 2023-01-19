e10298200eef0bf2baa0422772895699762f0c4a
OriSketchy <orisketchy@gmail.com>
2023-01-19 21:03:02 +0000

    Tweaked FollowPlayer script for Camera

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -8,11 +8,21 @@ public class FollowPlayer : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     public float smoothTime = 0.3f;
-
     private Vector3 velocity;
+    [SerializeField]
+    bool builtin = true;
 
     private void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothTime);
+        if(builtin)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothTime);
+        }
+        else
+        {
+            Vector3 distance = target.position + offset - transform.position;
+            Vector3 move = distance * smoothTime * Time.deltaTime;
+            transform.position += move;
+        }
     }
 }
