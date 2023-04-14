98b0d2fad180fa0096cace67ef27af5981bee009
OriSketchy <orisketchy@gmail.com>
2023-04-14 18:56:40 +0100

    Created main menu

---
+++ Assets/StartGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("aaaaaaaaaaaa");
+
+    }
+}
--- Assets/UI/Reset Level/Quit.cs
+++ Assets/UI/Reset Level/Quit.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quit : MonoBehaviour
 {
+    [SerializeField]
+    Button button;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
+
+
+
+    }
+    public void ButtonClicked()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+
     }
 }
